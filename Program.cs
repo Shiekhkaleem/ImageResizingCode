cc17116909ffe879da622ab7ad1848d5a6fb7f77
Shiekhkaleem <109513188+Shiekhkaleem@users.noreply.github.com>
2023-02-02 20:54:57 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿// 37345-12345-12345 valid
+
+// 4, 5, 37 => valid
+
+// 12:345-12345-12345 => not valid
+
+string[] tickets = {
+    "52345-12345-12345",
+    "370345-12345-12345",
+    "42345-12345-12345",
+    "345-12345-12345",
+    "92345-12345-12345",
+    "4245:12345:12345",
+    "37345-12345-12345",
+    "12345:12345-12345",
+};
+
+foreach (var item in tickets)
+{
+    var value = item.Split("-");
+
+    if (value.Length != 3)
+    {
+        Console.WriteLine("no");
+        continue;
+    }
+
+    for (int i = 0; i < value.Length; i++)
+    {
+        var word = value[i];
+
+        if (word.Length != 5)
+        {
+            Console.WriteLine("no");
+            break;
+        }
+
+        if (i == 0)
+        {
+            if(word[0] == '3')
+            {
+                if (word[1] != '7')
+                {
+                    Console.WriteLine("no");
+                    break;
+                }
+            } else
+            {
+                if (word[0] != '4' && word[0] != '5')
+                {
+                    Console.WriteLine("no");
+                    break;
+                }
+            }
+        }
+
+        if (i == value.Length - 1)
+        {
+            Console.WriteLine("yes");
+        }
+    }
+}
